f03fa98dda09671b1a5e8488145a0bba6eed2628
Antonio Juarez <jjuarez18@ucol.mx>
2019-08-20 09:52:06 -0500

    Agregue el diseño

---
+++ Loging/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Loging
+{
+    public partial class HarvardDataBase : Form
+    {
+        public HarvardDataBase()
+        {
+            InitializeComponent();
+        }
+
+        private void HarvardDataBase_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
